e50ef9ee65028bb15a2c8f8818c599b3483fd3f2
Ben Justol <benedictjustol10@gmail.com>
2026-05-03 09:32:31 -0400

    Created the interface, and each subclass.

--- extendLogMe/views/LogMe.java
+++ extendLogMe/views/LogMe.java
@@ -1,6 +1,11 @@
+import models.Task;
+
 public class LogMe {
     public static void main(String[] args)
     {
         System.out.println("Hello, Welcome to LogMe");
+
+        Task task = new Task("Dishes", 1);
+        System.out.println(task.toString());
     }
 }
