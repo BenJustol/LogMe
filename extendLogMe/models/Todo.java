350b867d54057a5a4fba7e9bdcef841ea18d41b8
Ben Justol <benedictjustol10@gmail.com>
2026-05-08 23:33:57 -0400

    Added more javadocs to Todo

--- extendLogMe/models/Todo.java
+++ extendLogMe/models/Todo.java
@@ -11,13 +11,25 @@ import java.util.Comparator;
 public class Todo implements DailyLog{
     private final ArrayList<Task> listOfTasks;
 
+    /**
+     * Default constructor that initializes a new list of task
+     *
+     * @post listOfTasks = #listOfTasks
+     * */
     public Todo()
     {
         listOfTasks = new ArrayList<>();
     }
 
     /**
-     * Method to addEntry into a ArrayList of Task, takes in String
+     * Adds a new Task into a ArrayList of Task, takes in String as param.
+     * Defaults the priority to 0 AND sorts the task after added to the list
+     *
+     * @param entry to set as the param for a creating a new task
+     *
+     * @pre entry != null
+     *
+     * @post listOfTask = new Task(entry, 0) AND listOfTask.sort()
      * */
     @Override
     public void addEntry(String entry)
@@ -27,23 +39,55 @@ public class Todo implements DailyLog{
         sort();
     }
 
+    /**
+     * Deletes a task from the ArrayList at the specified index in the list
+     *
+     * @param index position of task to be deleted in the list
+     *
+     * @return a boolean value if successful removal of a task in a list
+     *
+     * @throws IllegalArgumentException if index < 0 OR index >= listOfTask.size()
+     *
+     * @post result == true AND listOfTasks.size() == old(listOfTask.size()) - 1
+     * */
     @Override
-    public boolean deleteEntry(int index) throws IllegalArgumentException
+    public boolean deleteEntry(int index)
     {
-        if(index < 0 || index >= listOfTasks.size())
-            throw new IllegalArgumentException("Invalid index");
+        validateEntry(index);
         listOfTasks.remove(index);
         return true;
     }
 
+    /**
+     * Method to return a task private data as a String
+     *
+     * @param index of a task to delete in the list of Task
+     *
+     * @return String representation of a task data by calling the task toString function
+     *
+     * @pre index >= 0 AND index != null
+     *
+     * @post listOfTasks = #listOfTasks AND validateEntry(index) == true, return String representation of task
+     * */
     @Override
-    public String getEntry(int index) throws IllegalArgumentException
+    public String getEntry(int index)
     {
-        if(index < 0 || index >= listOfTasks.size())
-            throw new IllegalArgumentException("Invalid index");
+        validateEntry(index);
         return listOfTasks.get(index).toString();
     }
 
+    /**
+     * Method to updatePriority of a task based on its taskId
+     *
+     * @param taskId to find the value of a task in the list
+     *
+     * @param newPriority the value to update old(tasks.priority)
+     *
+     * @pre taskID >= 0 AND taskID != null. newPriority >= 0 AND newPriorty != null
+     *
+     * @post listOfTask = #listOfTask AND IFF taskId ! throw IllegalArguement Exception,
+     * old(datum.priority) = new(datum.priority). Sort() task in order
+     * */
     public void updatePriority(int taskId, int newPriority) throws IllegalArgumentException
     {
        for(Task datum : listOfTasks)
@@ -58,8 +102,21 @@ public class Todo implements DailyLog{
        throw new IllegalArgumentException("TaskID: " + taskId + " DNE");
     }
 
+    /**
+     * Sorts the listOfTask by completion AND priority
+     *
+     * @post listOfTasks becomes sors
+     * */
     public void sort()
     {
         listOfTasks.sort(Comparator.comparing(Task::isCompleted).thenComparingInt(Task::getPriority));
+    }
+
+    public void validateEntry(int index) throws IllegalArgumentException
+    {
+        if(index < 0 || index >= listOfTasks.size())
+        {
+            throw new IllegalArgumentException("Invalid Index...");
+        }
     }
 }
